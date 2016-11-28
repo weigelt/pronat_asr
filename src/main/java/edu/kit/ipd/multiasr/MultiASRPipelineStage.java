dce3c7b7c74a6d4acb7e65ac4ca6a175823d44d0
Sebastian Weigelt <weigelt@kit.edu>
2016-11-28 10:43:27 +0100

    add pipelinestage boiler plate

--- src/main/java/edu/kit/ipd/multiasr/MultiASRPipelineStage.java
+++ src/main/java/edu/kit/ipd/multiasr/MultiASRPipelineStage.java
@@ -1,10 +1,21 @@
 package edu.kit.ipd.multiasr;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.kit.ipd.parse.luna.data.AbstractPipelineData;
+import edu.kit.ipd.parse.luna.data.PipelineDataCastException;
+import edu.kit.ipd.parse.luna.data.PrePipelineData;
 import edu.kit.ipd.parse.luna.pipeline.IPipelineStage;
 import edu.kit.ipd.parse.luna.pipeline.PipelineStageException;
+import edu.kit.ipd.parse.multiasr.asr.ASROutput;
 import edu.kit.ipd.parse.multiasr.asr.GoogleASR;
 import edu.kit.ipd.parse.multiasr.asr.MultiASR;
 import edu.kit.ipd.parse.multiasr.asr.WatsonASR;
@@ -12,6 +23,10 @@ import edu.kit.ipd.parse.multiasr.asr.WatsonASR;
 public class MultiASRPipelineStage implements IPipelineStage {
 
 	private static final String ID = "multiasr";
+
+	private static final Logger logger = LoggerFactory.getLogger(MultiASRPipelineStage.class);
+
+	private PrePipelineData prePipeData;
 
 	private MultiASR multiASR;
 	private GoogleASR googleASR;
@@ -32,7 +47,32 @@ public class MultiASRPipelineStage implements IPipelineStage {
 
 	@Override
 	public void exec(AbstractPipelineData data) throws PipelineStageException {
-		// TODO Auto-generated method stub
+		// try to get data as pre pipeline data. If this fails, return
+		try {
+			prePipeData = data.asPrePipelineData();
+		} catch (final PipelineDataCastException e) {
+			logger.error("Cannot process on data - PipelineData unreadable", e);
+			throw new PipelineStageException(e);
+		}
+
+		URI uri;
+		try {
+			uri = this.getClass().getClassLoader().getResource("testaudio.flac").toURI();
+		} catch (final URISyntaxException e) {
+			// TODO message
+			e.printStackTrace();
+			throw new PipelineStageException();
+		}
+		final List<ASROutput> recognize = multiASR.recognize(Paths.get(uri));
+		final Iterator<ASROutput> outIterator = recognize.iterator();
+		if (outIterator.hasNext()) {
+			prePipeData.setMainHypotheses(outIterator.next());
+		} else {
+			logger.warn("No main Hypothesis!");
+		}
+		while (outIterator.hasNext()) {
+			prePipeData.addAltHypothesis(outIterator.next());
+		}
 
 	}
 
